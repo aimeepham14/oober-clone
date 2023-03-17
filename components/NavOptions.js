38cfcca710fc60d2af9cd23abf0ee1fa219835be
aimeepham14 <aimee.pham14@gmail.com>
2023-03-17 12:44:55 -0700

    added icons to homescreen

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,8 @@
 import { FlatList, Text, TouchableOpacity, View, Image } from 'react-native'
 import React from 'react';
 import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements"; 
+import { useNavigation } from "@react-navigation/native"
 
 const data = [
     {
@@ -19,19 +21,29 @@ const data = [
 
 
 const NavOptions = () => {
+    const navigation = useNavigation();
+
   return (
     <FlatList 
     data={data}
     keyExtractor={(item) => item.id}
     horizontal
     renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity 
+        onPress={() => navigation.navigate(item.screen)}
+        style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
             <View>
                 <Image
                 style={{ width: 120, height: 120, resizeMode: "contain"}}
                 source={{ uri: item.image }}
                 />
                 <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                <Icon 
+                style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                name="arrowright" 
+                color="white" 
+                type="antdesign"
+                />
             </View>
         </TouchableOpacity>
     )}
