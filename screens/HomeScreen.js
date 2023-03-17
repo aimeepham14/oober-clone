713b4268a3b45e837187a721017ca1697207ff73
aimeepham14 <aimee.pham14@gmail.com>
2023-03-17 13:21:42 -0700

    autocomplete done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,15 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react';
 import tw from "tailwind-react-native-classnames";
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
         <View style={tw`p-5`}>
@@ -17,6 +24,34 @@ const HomeScreen = () => {
                     uri:"https://links.papareact.com/gzs",
                 }}           
             />
+
+            <GooglePlacesAutocomplete 
+                placeholder="Where From?"
+                styles={{
+                    container: {
+                        flex: 0,
+                    },
+                    textInput: {
+                        fontSize: 18,
+                    },
+                }}
+                onPress={(data, details=null) => {
+                    console.log(data)
+                    console.log(details);
+                }}
+                fetchDetails={true}
+                returnKeyType={"search"}
+                enablePoweredByContainer={false}
+                minLength={2}
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    language: 'en'
+                }}
+                nearbyPlacesAPI='GooglePlacesSearch'
+                debounce={400}
+            />
+
+
             <NavOptions />
         </View>
     </SafeAreaView>
