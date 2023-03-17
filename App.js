e5a9a8bede3bd523fdafb43c706a8ea45c50426e
aimeepham14 <aimee.pham14@gmail.com>
2023-03-17 12:19:39 -0700

    built navoptions

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // 1. Set up redux
 
@@ -10,7 +11,9 @@ import { store } from './store';
 export default function App() {
   return (
     <Provider store={store}>
+      <SafeAreaProvider>
         <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
