f78dc116b47bdec32a2e9a191dfd212e93e6f755
aimeepham14 <aimee.pham14@gmail.com>
2023-03-20 13:35:12 -0700

    added nav favorites

--- components/NavFavorites.js
+++ components/NavFavorites.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { FlatList } from 'react-native-gesture-handler';
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
 
 const data = [
     {
@@ -21,10 +23,25 @@ const NavFavorites = () => {
   return (  
     <FlatList 
         data={data} 
-        keyExtractor={(item) => item.id} 
-        renderItem={({item}) => (
-            <TouchableOpacity>
-                <Text>Yo</Text>
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={() => (
+            <View 
+                style={[tw`bg-gray-200 h-1`, { height: 0.5 }]}
+            />
+        )}
+        renderItem={({item: { location, destination, icon}}) => (
+            <TouchableOpacity style={tw`flex-row items-center p-5`}>
+                <Icon 
+                    style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+                    name={icon}
+                    type="ionicon"
+                    color="white"
+                    size={18}
+                />
+                <View>
+                    <Text style={tw`font-semibold text-lg`}>{location}</Text>
+                    <Text style={tw`text-gray-500`}>{destination}</Text>
+                </View>
             </TouchableOpacity>
         )}
     />
