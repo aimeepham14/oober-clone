4e5576ae6bcfc9c7524a6e767d4bf63ad29e8162
aimeepham14 <aimee.pham14@gmail.com>
2023-03-20 15:18:21 -0700

    done with app

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,35 +1,41 @@
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import tw from "tailwind-react-native-classnames";
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import { Icon } from "react-native-elements";
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectTravelTimeInformation } from '../slices/navSlice';
 
 
 const data =[
   {
     id: "Uber-X-123",
-    title: "UberX",
+    title: "OoberX",
     multiplier: 1,
     image: "https://links.papareact.com/3pn",
   },
   {
     id: "Uber-XL-456",
-    title: "Uber XL",
+    title: "Oober XL",
     multiplier: 1.2,
     image: "https://links.papareact.com/5w8",
   },
   {
     id: "Uber-LUX-789",
-    title: "Uber LUX",
+    title: "Oober LUX",
     multiplier: 1.75,
     image: "https://links.papareact.com/7pf",
   },
 ];
 
+// If we have surge pricing ,this goes up
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
 
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
@@ -40,7 +46,7 @@ const RideOptionsCard = () => {
         >
           <Icon name="chevron-left" type="fontawesome"/>
         </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride - {travelTimeInformation?.distance?.text}</Text>
       </View>
 
       <FlatList
@@ -60,14 +66,21 @@ const RideOptionsCard = () => {
             />
             <View style={tw`-ml-6`}>
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>Travel time...</Text>
+              <Text>{travelTimeInformation?.duration?.text} Travel Time</Text>
             </View>
-            <Text style={tw`text-xl`}>$40</Text>
+            <Text style={tw`text-xl`}>
+                {new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
+                }).format(
+                  (travelTimeInformation?.duration.value * SURGE_CHARGE_RATE * multiplier) / 100 
+                )}
+            </Text>
           </TouchableOpacity>
         )} 
       />
 
-      <View>
+      <View style={tw`mt-auto border-t border-gray-200`}>
         <TouchableOpacity 
           disabled={!selected} 
           style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -7,6 +7,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useNavigation } from '@react-navigation/native';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { Icon } from "react-native-elements";
+
 
 const MapScreen = () => {
     const Stack = createNativeStackNavigator();
@@ -14,6 +16,12 @@ const MapScreen = () => {
 
   return (
     <View>
+
+        <TouchableOpacity 
+          onPress={() => navigation.navigate("HomeScreen")}
+          style={tw`absolute top-16 left-8 bg-gray-100 z-50 p-3 rounded-full shadow-lg`}>
+          <Icon name="menu" />
+        </TouchableOpacity>
         <View style={tw`h-1/2`}>
             <Map /> 
         </View>
